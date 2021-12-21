2770b4fd85b227b99abba7751ceda275b4a3bf35
VladimirTsvetov <v.a.tsvetov@gmail.com>
2021-12-21 23:22:43 +0300

    operator [] overrides example

---
+++ MyArray.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+class MyArray
+{
+    int* buff;
+    int size;
+public:
+    MyArray() {
+        buff = nullptr;
+        size = 0;
+    }
+    MyArray(int s) :size(s) {
+        buff = new int[size];
+        for (int i = 0; i < size; i++)buff[i] = 0;
+    }
+    ~MyArray() {
+        if (buff)delete[] buff;
+    }
+    int& operator[](int index) {
+        return buff[index];
+    }
+
+};
+
+
+int main()
+{
+    MyArray arr(10);
+    arr[4] = 222;
+    arr[4]++;
+    std::cout << "arr[4] = " << arr[4] << std::endl;
+    std::cout << "Hello World!\n";
+}
